7db112dcd4081400adf0aebde58c943646f86faf
David Kostka <dkostka@eurofunk.com>
2022-02-03 20:58:25 +0100

    Initial commit

---
+++ src/main/java/cz/kostka/pochod/PochodApplication.java
@@ -0,0 +1,13 @@
+package cz.kostka.pochod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PochodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PochodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/kostka/pochod/PochodApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.kostka.pochod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
