6cd1132a1e8c4fafbb471cb97a0205de7df0fe5d
David Kostka <dkostka@eurofunk.com>
2022-06-21 12:43:27 +0200

    Add logs for successful login

--- src/main/java/cz/kostka/pochod/controller/LoginController.java
+++ src/main/java/cz/kostka/pochod/controller/LoginController.java
@@ -1,6 +1,9 @@
 package cz.kostka.pochod.controller;
 
+import cz.kostka.pochod.security.CustomUserDetails;
 import cz.kostka.pochod.util.LoginUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
 
     @GetMapping("/login")
     public String login() {
@@ -27,6 +31,8 @@ public class LoginController {
     @GetMapping("/successfulLogin")
     public String redirectUserBasedOnRole() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LOG.info("User '{}' is successfully logged in.",
+                ((CustomUserDetails) authentication.getPrincipal()).getUser().getUsername());
         return LoginUtils.getRedirectUrlAfterLoginForRole(authentication.getAuthorities());
     }
 }
