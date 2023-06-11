9da2940b3af64562a197e957c62126175834642c
David Kostka <dkostka@eurofunk.com>
2023-06-11 15:08:20 +0200

    Add possibility to download diplom when all stamps are taken for user #15

---
+++ src/main/java/cz/kostka/pochod/controller/DiplomController.java
@@ -0,0 +1,45 @@
+package cz.kostka.pochod.controller;
+
+import cz.kostka.pochod.security.CustomUserDetails;
+import cz.kostka.pochod.service.DiplomService;
+import cz.kostka.pochod.service.GameInfoService;
+import cz.kostka.pochod.service.PlayerService;
+import cz.kostka.pochod.service.StampService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/pop/diplom")
+public class DiplomController extends PlayerController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DiplomController.class);
+
+    private final DiplomService diplomService;
+
+    @Autowired
+    public DiplomController(
+            final PlayerService playerService,
+            final StampService stampService,
+            final GameInfoService gameInfoService,
+            final DiplomService diplomService) {
+        super(playerService, stampService, gameInfoService);
+        this.diplomService = diplomService;
+    }
+
+    @GetMapping
+    public void download(
+            @AuthenticationPrincipal final CustomUserDetails user, final HttpServletResponse response)
+            throws IOException {
+        LOG.info("User '{}' tries to download a diplom.", user.getUsername());
+
+        diplomService.download(user.getUsername(), response);
+    }
+}
